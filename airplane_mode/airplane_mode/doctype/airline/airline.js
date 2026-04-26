aed19e6e135d32b152aa1f161e08b3c9d01d5d8b
Pritish Bhatia <pritiah.bhatia@promantia.com>
2026-04-26 15:36:30 +0530

    Added role permissions and restricted audit field access

--- airplane_mode/airplane_mode/doctype/airline/airline.js
+++ airplane_mode/airplane_mode/doctype/airline/airline.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2026, pritish and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airline", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airline", {
+    refresh: function(frm) {
 
-// 	},
-// });
+        if (frm.doc.website) {
+
+            frm.add_web_link(
+                frm.doc.website,
+                "Visit Website"
+            );
+
+        }
+
+    }
+});
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,20 @@
 // Copyright (c) 2026, pritish and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+    refresh: function(frm) {
 
-// 	},
-// });
+        frm.add_custom_button("Assign Seat", function() {
+
+            frappe.prompt(
+                "Enter Seat Number",
+                function(values) {
+                    frm.set_value("seat", values.value);
+                },
+                "Assign Seat"
+            );
+
+        }, "Actions");
+
+    }
+});
